ab1095b1016da2d259f9cdeeaa0306e153de8d1c
MichaelMW <mwang@genecehealth.com>
2023-12-24 21:58:52 -0800

    support port for sshNoPsswd

--- sshNoPsswd.sh
+++ sshNoPsswd.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 
-### TODO: 1. add support for port eg. -p 1234
+# This script automatically adds an authorized key to a remote server
 
-### TODO: 2. add alias for $1, and add to known host
+# Usage: script_name user@server [-p port]
 
-# this script automatically at authorized key to remote server
+# Default SSH port
+PORT=22
+
+# Parse options
+while getopts ":p:" opt; do
+  case ${opt} in
+    p )
+      PORT=$OPTARG
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    : )
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+# Assign user@server to a variable
+SSH_TARGET=$1
+
+# Check if SSH key exists, generate if not
 if [ ! -f ~/.ssh/id_rsa.pub ]; then
-	ssh-keygen -t rsa
+    ssh-keygen -t rsa
 fi
 
-## $1 is user@server
-ssh $1 mkdir -p .ssh
-cat ~/.ssh/id_rsa.pub | ssh $1 'cat >> .ssh/authorized_keys'
-ssh $1 "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
+# Add public key to remote server's authorized_keys
+ssh -p $PORT $SSH_TARGET mkdir -p .ssh
+cat ~/.ssh/id_rsa.pub | ssh -p $PORT $SSH_TARGET 'cat >> .ssh/authorized_keys'
+ssh -p $PORT $SSH_TARGET "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 
